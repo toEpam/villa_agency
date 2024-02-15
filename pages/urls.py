f80c2696416686c51d154161654e6d154fafffe2
user <uznext17@gmail.com>
2024-02-16 00:53:45 +0500

    .

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, ContactPageView, PropertiesPageView, PropertyDetailsPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('contact', ContactPageView.as_view(), name='contact'),
+    path('properties', PropertiesPageView.as_view(), name='properties'),
+    path('property_details', PropertyDetailsPageView.as_view(), name='property_details'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,27 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
+    extra_context = {
+        'title': 'Villa Agency - Real Estate',
+    }
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+    extra_context = {
+        'title': 'Villa Agency - Contact Page',
+        'page_heading': 'Contact Us'
+    }
+
+class PropertiesPageView(TemplateView):
+    template_name = 'properties.html'
+    extra_context = {
+        'title': 'Villa Agency - Property Listing',
+        'page_heading': 'Properties'
+    }
+
+class PropertyDetailsPageView(TemplateView):
+    template_name = 'property-details.html'
+    extra_context = {
+        'title': 'Villa Agency - Property Detail Page',
+        'page_heading': 'Single Property'
+    }
