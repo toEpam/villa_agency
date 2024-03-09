fd5edc9bd16363bb343b12585802ef03481c5cd6
toEpam <toepammiddle@gmail.com>
2024-03-10 01:38:08 +0500

    .

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', include('pages.urls')),
 ]
 
-if True:
+if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
