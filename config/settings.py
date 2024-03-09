229e7976cbd48e5d214e7c4ff2434ab933872627
toEpam <toepammiddle@gmail.com>
2024-03-10 04:59:02 +0500

    .

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", subcast=str)
+# ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", subcast=str)
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
