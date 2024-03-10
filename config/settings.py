8b899f5d4c4abbbafa387dfaeebb85dec4182efa
toEpam <toepammiddle@gmail.com>
2024-03-10 11:37:08 +0500

    .

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY='django-insecure-kaha)s7ln+lfrj$gegl)q3*$wxbkery$9idhvldp3(n=ttbfw0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
